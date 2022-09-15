160d56703b62cb871324dca3b3ca6339dfae6698
Luke Le <70491331+Ricealicious@users.noreply.github.com>
2022-09-15 12:22:09 -0500

    initial linear search algorithm

--- linear_search.c
+++ linear_search.c
@@ -13,6 +13,28 @@
  * The algorithm was implemented in C due to how both the programming language and algorithm
  * parallel one another in basic concept. Both are very simple. It just felt fitting.
  */
+#include <stdio.h>        /* - The library contains the printf function, which will be used to send
+                           *   an output to stdout
+                           * - Contains sizeof function. When given an argument, returns the size in
+                           *   the unit of bytes. 
+                           */
+
+/**
+ * @brief Linear search algorithm. Returns the position of the desired element in the array.
+ * 
+ * @param input 
+ * @param n 
+ * @param identify 
+ * @return int 
+ */
+int linearSearch(int input[], int n, int identify) {
+  for(int i = 0; i < n; i++) {
+    if (*(input + i) == identify)
+      return i;
+  }
+
+  return -1;
+}
 
 /**
  * @brief The driver code. The code initializes an array that is to act as a test input for the 
@@ -22,5 +44,13 @@
  * @return int 
  */
 int main() {
+  int input[]  = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};    // Array of size n
+  int n = sizeof(input)/sizeof(input[0]);           // Size of array
+  int identify = 9;                                 // Desired element
 
+  int position = linearSearch(input, n, identify);  // passes array to Linear Search for processing
+  printf("The element is at position %d",    // outputs the position of the searched element
+  position);
+  
+  return 0;
 }
