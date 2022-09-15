da87239e12b981d00115d14fb0624554b53261d4
Luke Le <70491331+Ricealicious@users.noreply.github.com>
2022-09-15 12:38:05 -0500

    complete documentation

--- linear_search.c
+++ linear_search.c
@@ -1,6 +1,7 @@
 /* 
  * In order to manifest and develop a passion for algorithms and the logic behind them, I have 
- * decided to try to study an algorithm every day. The algorithms will be implemeneted in an
+ * decided to try to study algorithm every daym and have a reasonable understanding of its
+ * implementation and tradeoffs within the week. The algorithms will be implemeneted in an
  * arbitrary langauge, with likely aribitrary libraries, given that I am to provide a reason 
  * for its usage. This is largely so that I am forced to develop a deeper understanding of the 
  * tools that I use. The first algorithm in this collection will be the implementation of a 
@@ -20,7 +21,13 @@
                            */
 
 /**
- * @brief Linear search algorithm. Returns the position of the desired element in the array.
+ * @brief Linear search algorithm. Returns the position of the desired element in the array. 
+ *
+ * The algo has the running time O(n), as in the worst case that the searched element is located at 
+ * the end of the data set, the array is required to traverse from the beginning towards the end 
+ * through each individual elementWhile this isn't an issue for smaller data sets, the algorithm 
+ * will likely be noticeably inefficient for larger data sets. In the best case, the element is 
+ * conveniently placed at the front which has a running time of O(1).
  * 
  * @param input 
  * @param n 
@@ -28,8 +35,10 @@
  * @return int 
  */
 int linearSearch(int input[], int n, int identify) {
-  for(int i = 0; i < n; i++) {
-    if (*(input + i) == identify)
+  for(int i = 0; i < n; i++) {        // traverses through the array, given the array's address
+    if (*(input + i) == identify)     /* the compiler understands that incrementing an array's
+                                       * address is to move along the array relative to the 
+                                       * address */
       return i;
   }
 
@@ -44,12 +53,12 @@ int linearSearch(int input[], int n, int identify) {
  * @return int 
  */
 int main() {
-  int input[]  = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};    // Array of size n
-  int n = sizeof(input)/sizeof(input[0]);           // Size of array
-  int identify = 9;                                 // Desired element
+  int input[]  = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};       // Array of size n
+  int n = sizeof(input)/sizeof(input[0]);              // Size of array
+  int identify = 9;                                    // Desired element
 
-  int position = linearSearch(input, n, identify);  // passes array to Linear Search for processing
-  printf("The element is at position %d",    // outputs the position of the searched element
+  int position = linearSearch(input, n, identify);     // passes array to Linear Search for processing
+  printf("The element is at position %d \n",    // outputs the position of the searched element
   position);
   
   return 0;
