338cd2d51bbd2a3fb2e84ed3248ab100d525f8a7
Luke Le <70491331+Ricealicious@users.noreply.github.com>
2022-09-15 11:39:50 -0500

    initial manifesto and linear search comments

---
+++ linear_search.c
@@ -0,0 +1,18 @@
+/* 
+ * In order to manifest and develop a passion for algorithms and the logic behind them, I have 
+ * decided to try to study an algorithm every day. The algorithms will be implemeneted in an
+ * arbitrary langauge, with likely aribitrary libraries, given that I am to provide a reason 
+ * for its usage. This is largely so that I am forced to develop a deeper understanding of the 
+ * tools that I use. The first algorithm in this collection will be the implementation of a 
+ * simple algorithm: Linear Search.
+ *
+ * Linear Search is likely the easiest algorithm to implement while also one of the most efficient 
+ * given that the input size is reasonably small. The algorithm traverses through each individual
+ * element in a data set until the desired element has been identified.
+ *
+ * The algorithm was implemented in C due to how both the programming language and algorithm
+ * parallel one another in basic concept. Both are very simple. It just felt fitting.
+ */
+int main() {
+    
+}
