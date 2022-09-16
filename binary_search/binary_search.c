aaae011e05d637b7b8c50fe55d4b5ecfc3523adf
Luke Le <70491331+Ricealicious@users.noreply.github.com>
2022-09-16 14:51:24 -0500

    initial comments

---
+++ binary_search/binary_search.c
@@ -0,0 +1,15 @@
+/**
+ * @file binary_search.c
+ * @author Luke Le
+ * @brief An implementation of the binary search algorithm. Due to the nature of the algorithm, there is actually
+ *        multiple approaches to implementing the algorithm. It's simplicity allows for the algorithm to be
+ *        to be implemented following an iterative method, but its repetitive sectioning suggests a potential for 
+ *        a recursive implementation.
+ *        The algorithm is to be implemented in C so that I may further practice using the programming language.
+ * @version 0.1
+ * @date 2022-09-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
